9ea4bea3b752919c5415b3d65c7511c2bbbc6208
ekh <52539130+eomkyeongho@users.noreply.github.com>
2022-02-23 06:56:32 +0900

    넉백 + 피격효과 + 파일정리

---
+++ Assets/Scripts/Monster/Monster.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Transform target;
+    SpriteRenderer sprite;
+    BoxCollider2D boxCollider;
+    bool isDamaged;
+
+    public void init(GameObject obj)
+    {
+        rigid = obj.GetComponent<Rigidbody2D>();
+        sprite = obj.GetComponent<SpriteRenderer>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        boxCollider = obj.GetComponent<BoxCollider2D>();
+        isDamaged = false;
+    }
+    public void Move(float moveSpeed)
+    {
+        if(!isDamaged)
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+        if (rigid.position.x > target.position.x)
+            sprite.flipX = true;
+        else if (rigid.position.x < target.position.x)
+            sprite.flipX = false;
+    }
+    
+    public void KnowBackAwayFromMaterial(Collider2D collision, float power, float durationTime)
+    {
+        Vector2 reactVec = transform.position - collision.transform.position;
+        StartCoroutine(OnDamage(reactVec, power, durationTime));
+    }
+
+    public void KnowBackAwayFromMaterial(GameObject obj, Collider2D collision, float power, float durationTime)
+    {
+        Vector2 reactVec = transform.position - collision.transform.position;
+        StartCoroutine(OnDamage(obj, reactVec, power, durationTime));
+    }
+
+    public void KnowBackAwayFromPlayer(Collider2D collision, float power, float durationTime)
+    {
+        Vector2 reactVec = transform.position - target.position;
+        StartCoroutine(OnDamage(reactVec, power, durationTime));
+        StartCoroutine(OnDamageEffect());
+    }
+
+    public void KnowBackAwayFromPlayer(GameObject obj, Collider2D collision, float power, float durationTime)
+    {
+        Vector2 reactVec = transform.position - target.position;
+        StartCoroutine(OnDamage(obj, reactVec, power, durationTime));
+    }
+
+    IEnumerator OnDamage(Vector2 reactVec, float power, float durationTime)
+    {
+        isDamaged = boxCollider.isTrigger = true;
+        reactVec = reactVec.normalized;
+        rigid.velocity = reactVec * power;
+        yield return new WaitForSeconds(durationTime);
+        rigid.velocity = new Vector2(0,0);
+        isDamaged = boxCollider.isTrigger = false;
+    }
+
+    IEnumerator OnDamage(GameObject obj, Vector2 reactVec, float power, float durationTime)
+    {
+        obj.GetComponent<Animator>().SetBool("Damaged", true);
+        isDamaged = boxCollider.isTrigger = true;
+        reactVec = reactVec.normalized;
+        rigid.velocity = reactVec * power;
+        yield return new WaitForSeconds(durationTime);
+        rigid.velocity = new Vector2(0, 0);
+        isDamaged = boxCollider.isTrigger = false;
+        obj.GetComponent<Animator>().SetBool("Damaged", false);
+    }
+
+    public IEnumerator OnDamageEffect()
+    {
+        float durationTime = 0.01f;
+
+        while (durationTime > 0)
+        {
+            sprite.color = Color.red;
+            yield return new WaitForSeconds(0.05f);
+            sprite.color = Color.white;
+            yield return new WaitForSeconds(0.05f);
+            durationTime -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Monster/Slime.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : Monster
+{
+    Monster m_Monster;
+
+    [Header("추격 속도")]
+    [SerializeField] [Range(1f, 4f)] float moveSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Monster = GetComponent<Monster>();
+        m_Monster.init(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Monster.Move(moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Ball2")
+        {
+            m_Monster.KnowBackAwayFromPlayer(gameObject, collision, 5.0f, 0.4f);
+        }
+    }
+}
