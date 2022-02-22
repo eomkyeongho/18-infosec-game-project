edcdd922a3082b393fe44b2deb7f47cc7e0869c2
ekh <52539130+eomkyeongho@users.noreply.github.com>
2022-02-23 05:07:22 +0900

    넉백 자연스럽게 + 피격 효과

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,54 +4,73 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    Rigidbody2D rb;
+    Rigidbody2D rigid;
     Transform target;
-    SpriteRenderer sr;
+    SpriteRenderer sprite;
+    BoxCollider2D boxCollider;
 
     [Header("추격 속도")]
     [SerializeField] [Range(1f, 4f)] float moveSpeed = 3f;
 
+    bool isDamaged;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        isDamaged = false;
     }
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        if(!isDamaged)
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
 
-        if (rb.position.x > target.position.x)
-            sr.flipX = true;
-        else if (rb.position.x < target.position.x)
-            sr.flipX = false;
-    }
-    
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "monster" && rb)
-            rb.velocity = new Vector2(0, 0);
+        if (rigid.position.x > target.position.x)
+            sprite.flipX = true;
+        else if (rigid.position.x < target.position.x)
+            sprite.flipX = false;
     }
     
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Ball" && rb)
+        if(collision.tag == "Ball" && rigid)
         {
             Vector2 reactVec = transform.position - collision.transform.position;
-            StartCoroutine(OnDamage(reactVec, 30.0f));
+            StartCoroutine(OnDamage(reactVec, 5.0f));
         }
 
-        if (collision.tag == "Ball2" && rb)
+        if (collision.tag == "Ball2" && rigid)
         {
             Vector2 reactVec = transform.position - target.position;
-            StartCoroutine(OnDamage(reactVec, 50.0f));
+            StartCoroutine(OnDamage(reactVec, 10.0f));
+            StartCoroutine(OnDamageEffect());
         }
     }
     IEnumerator OnDamage(Vector2 reactVec, float power)
     {
+        isDamaged = true;
+        boxCollider.isTrigger = true;
         reactVec = reactVec.normalized;
-        rb.velocity = reactVec * power;
+        rigid.velocity = reactVec * power;
         yield return new WaitForSeconds(0.2f);
-        rb.velocity = new Vector2(0,0);
+        rigid.velocity = new Vector2(0,0);
+        boxCollider.isTrigger = false;
+        isDamaged = false;
+    }
+
+    IEnumerator OnDamageEffect()
+    {
+        float durationTime = 0.01f;
+
+        while (durationTime > 0)
+        {
+            sprite.color = Color.red;
+            yield return new WaitForSeconds(0.05f);
+            sprite.color = Color.white;
+            yield return new WaitForSeconds(0.05f);
+            durationTime -= Time.deltaTime;
+        }
     }
 }
